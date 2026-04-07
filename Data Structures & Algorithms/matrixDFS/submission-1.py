3e9eb80995bf45ee1eb64d92bdc5f245528b2bb7
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-04-07 22:49:54 +0100

    Add: matrixDFS - submission-1

---
+++ Data Structures & Algorithms/matrixDFS/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countPaths(self, grid: List[List[int]]) -> int:
+        count = 0
+        if not grid:
+            return count
+
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def dfs(grid, r, c, visited):
+            nonlocal count
+            if min(r, c) < 0 or r == rows or c == cols or grid[r][c] == 1 or (r, c) in visited:
+                return
+
+            if r == rows - 1 and c == cols - 1:
+                count += 1
+                return
+
+            visited.add((r, c))
+
+            directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+            for dr, dc in directions:
+                dfs(grid, r + dr, c + dc, visited.copy())
+
+        dfs(grid, 0, 0, set())
+        return count
