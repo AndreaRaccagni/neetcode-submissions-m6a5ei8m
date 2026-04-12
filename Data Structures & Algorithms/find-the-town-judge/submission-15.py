265f6e54eb22f3f5dfbc80de349e5e92b7cb6df2
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-04-12 11:15:56 +0100

    Add: find-the-town-judge - submission-15

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-15.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        count = {}
+        voters = set()
+        judge = 0
+
+        for voter, voted in trust:
+            voters.add(voter)
+            count[voted] = count.get(voted, 0) + 1
+
+        for voted, votes in count.items():
+            if votes == n - 1 and voted not in voters:
+                return voted
+        
+        return -1
+        
