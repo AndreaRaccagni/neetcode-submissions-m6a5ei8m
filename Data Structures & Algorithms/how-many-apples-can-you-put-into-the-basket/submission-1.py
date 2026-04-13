84c6980afd3f2a3a002e270e45786584933876fa
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-04-13 18:08:43 +0100

    Add: how-many-apples-can-you-put-into-the-basket - submission-1

---
+++ Data Structures & Algorithms/how-many-apples-can-you-put-into-the-basket/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxNumberOfApples(self, weight: List[int]) -> int:
+        weight.sort()
+        totalWeight = 0
+        apples = 0
+        
+        for w in weight:
+            if w + totalWeight > 5000:
+                break
+            else:
+                totalWeight += w
+                apples += 1
+
+        return apples
