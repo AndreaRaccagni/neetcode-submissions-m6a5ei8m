6b9c41009113c0ae918e4936a1f8e01a768e883e
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-04-26 23:29:38 +0100

    Add: is-palindrome - submission-13

---
+++ Data Structures & Algorithms/is-palindrome/submission-13.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+
+        while l <= r:
+            left = s[l].lower()
+            right = s[r].lower()
+
+            if not self.isCharValid(left):
+                l += 1
+                continue
+
+            if not self.isCharValid(right):
+                r -= 1
+                continue
+
+            if left != right:
+                print(left, right)
+                return False
+
+            r -= 1
+            l += 1
+
+        return True
+
+    def isCharValid(self, c):
+        return 'a' <= c <= 'z' or '0' <= c <= '9'
