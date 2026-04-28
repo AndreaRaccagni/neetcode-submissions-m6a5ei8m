ca635afc8969f358e970aeb98512e8b9f2d432d7
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-04-28 19:30:37 +0100

    Add: evaluate-reverse-polish-notation - submission-22

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-22.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        const stack = []
+        const ops = new Set(['+', '-', '*', '/'])
+
+        for (const t of tokens) {
+            if (!ops.has(t)) {
+                stack.push(parseInt(t))
+                continue
+            }
+
+            const b = stack.pop()
+            const a = stack.pop()
+            let res = 0
+
+            switch(t) {
+                case '+':
+                    res = a + b
+                    break
+                case '-':
+                    res = a - b
+                    break
+                case '*':
+                    res = a * b
+                    break
+                default:
+                    res = Math.trunc(a / b)
+            }
+            stack.push(res)
+        }
+
+        return stack.pop()
+    }
+}
