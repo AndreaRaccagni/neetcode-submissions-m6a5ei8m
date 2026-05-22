12ff92172732e8815338f94e2030261ecfb5473a
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-05-22 09:54:27 +0100

    Add: find-target-in-rotated-sorted-array - submission-25

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-25.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0
+        let r = nums.length - 1
+
+        while (l <= r) {
+            const mid = Math.floor((r - l) / 2) + l
+
+            if (nums[mid] === target) {
+                return mid
+            } else if (nums[l] <= nums[mid]) {
+                if (target >= nums[l] && target <= nums[mid]) {
+                    r = mid
+                } else {
+                    l = mid + 1
+                }
+            } else {
+                if (target >= nums[mid] && target <= nums[r]) {
+                    l = mid
+                } else {
+                    r = mid - 1
+                }
+            }
+        }
+
+        return -1
+    }
+}
