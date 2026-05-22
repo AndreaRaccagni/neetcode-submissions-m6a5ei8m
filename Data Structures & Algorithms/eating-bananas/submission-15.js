7bb1608f64ff475a87ae2f90905e0f2a16d91199
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-05-22 09:42:20 +0100

    Add: eating-bananas - submission-15

---
+++ Data Structures & Algorithms/eating-bananas/submission-15.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let l = 1
+        let r = Math.max(...piles)
+
+        while (l < r) {
+            const mid = Math.floor((r - l) / 2) + l
+
+            let k = 0
+            for (const pile of piles) {
+                k += Math.ceil(pile / mid)
+            }
+
+            console.log(k)
+
+            if (k > h) {
+                l = mid + 1
+            } else {
+                r = mid
+            }
+        }
+
+        return l
+
+    }
+}
