b8310b0afec44366987b6a5d61ea6dca2e96329e
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-05-27 11:08:53 +0100

    Add: clone-graph - submission-4

---
+++ Data Structures & Algorithms/clone-graph/submission-4.js
@@ -0,0 +1,39 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        if (!node) {
+            return node
+        }
+
+        const oldToNew = new Map()
+
+        const dfs = (node) => {
+            if (oldToNew.has(node)) {
+                return oldToNew.get(node)
+            }
+
+            const newNode = new Node(node.val)
+            oldToNew.set(node, newNode)
+
+            for (const n of node.neighbors) {
+                newNode.neighbors.push(dfs(n))
+            }
+            return newNode
+        }
+
+        return dfs(node)
+    }
+}
