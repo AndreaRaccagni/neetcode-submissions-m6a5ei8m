0fd87d523a4c47fb0de8974612017a14d5179e6d
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-05-31 20:31:31 +0100

    Add: concatenation-of-array - submission-7

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-7.js
@@ -0,0 +1,18 @@
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        const n = nums.length
+        const res = new Array(n * 2).fill(0)
+
+
+        for (let i = 0; i < n; i++) {
+            res[i] = res[i + n] = nums[i]
+        }
+
+        return res
+    }
+}
