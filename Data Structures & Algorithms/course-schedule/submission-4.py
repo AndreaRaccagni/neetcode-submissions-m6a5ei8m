fad593de26141bb3cd062bbdd718b78c2adb86f8
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-06-02 11:36:50 +0100

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        courses = {}
+
+        for c, pre in prerequisites:
+            if c not in courses:
+                courses[c] = []
+
+            courses[c].append(pre)
+
+        visiting = set()
+
+        def dfs(curr):
+            if curr in visiting:
+                return False
+            if not courses.get(curr):
+                return True
+
+            visiting.add(curr)
+            for pre in courses[curr]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(curr)
+            del courses[curr] 
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+
+        return True
