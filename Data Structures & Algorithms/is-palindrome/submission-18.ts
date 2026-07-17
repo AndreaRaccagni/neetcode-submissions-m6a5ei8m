e54c2021a094f1523592f69ac901ca76080aa504
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-07-17 18:41:49 +0100

    Add: is-palindrome - submission-18

---
+++ Data Structures & Algorithms/is-palindrome/submission-18.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        let l = 0
+        let r = s.length - 1
+
+        while (l < r) {
+            if (!this.isAlphaNum(s[l])) {
+                l++
+            } else if (!this.isAlphaNum(s[r])) {
+                r--
+            } else {
+                if (s[l].toLowerCase() != s[r].toLowerCase()) {
+                    return false
+                }
+                l++
+                r--
+            }
+        }
+        return true
+    }
+
+
+    isAlphaNum(c: string): boolean {
+        const regex = /^[A-Za-z0-9]$/
+        return regex.test(c)
+    }
+}
