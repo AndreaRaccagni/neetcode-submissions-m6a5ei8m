c03171c57b1c930dc5f16d8414cc8e858e117ab6
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-07-22 19:08:42 +0100

    Add: invert-a-binary-tree - submission-24

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-24.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+    invertTree(root: TreeNode | null): TreeNode {
+
+        function dfs(node: TreeNode | null): null {
+            if (!node) return null
+
+            const tmp = node.left
+            node.left = node.right
+            node.right = tmp
+
+            dfs(node.left)
+            dfs(node.right)
+        }
+
+        dfs(root)
+        return root
+    }
+}
