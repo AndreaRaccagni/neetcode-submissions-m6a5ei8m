9fb8ff2fdc485c1052f1d31a5c70240cb53f8816
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-07-22 19:00:35 +0100

    Add: longest-substring-without-duplicates - submission-11

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-11.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+        let l = 0
+        const seen = new Set()
+        let maxLen = 0
+        
+        for (let r = 0; r < s.length; r++) {
+            while (seen.has(s[r])) {
+                seen.delete(s[l])
+                l++
+            }
+            seen.add(s[r])
+            maxLen = Math.max(maxLen, r - l + 1)
+        }
+
+        return maxLen
+    }
+}
