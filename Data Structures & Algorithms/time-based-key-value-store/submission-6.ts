d049dc2a255ca124e32205fd8f451133908c827a
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-08-04 10:45:00 +0100

    Add: time-based-key-value-store - submission-6

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-6.ts
@@ -0,0 +1,59 @@
+class TimeMap {
+    keyStore: Map<string, { timestamp: number, value: string }[]>
+
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key: string, value: string, timestamp: number): void {
+        const values = this.keyStore.get(key)
+        if (values) {
+            values.push({ timestamp, value })
+        } else {
+            this.keyStore.set(key, [{ timestamp, value }])
+        }
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key: string, timestamp: number): string {
+        const currValues = this.keyStore.get(key)
+        if (!currValues || !currValues.length) {
+            return ''
+        }
+        
+        return this.getValue(currValues, timestamp)
+
+    }
+
+    private getValue(values: { timestamp: number, value: string }[], timestamp: number): string {
+        let l = 0
+        let r = values.length - 1
+        let target = -1
+
+        while (l <= r) {
+            const mid = Math.floor((r - l) / 2) + l
+
+            if (values[mid].timestamp === timestamp) {
+                return values[mid].value
+            }
+
+            if (values[mid].timestamp < timestamp) {
+                target = mid
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }
+        }
+        return target !== -1 ? values[target].value : ''
+    }
+}
