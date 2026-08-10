457572ac24e1cf7f4111089a6caddca897675d2f
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-08-10 10:10:34 +0100

    Add: longest-common-prefix - submission-16

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-16.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs: string[]): string {
+        let prefix = strs[0]
+
+        for (const s of strs) {
+            prefix = this.findCommonSubstring(prefix, s)
+        }
+        return prefix
+    }
+
+    findCommonSubstring(w1: string, w2: string) {
+        let p = 0
+        while (p < w1.length && p < w2.length) {
+            if (w1[p] !== w2[p]) {
+                return w1.slice(0, p)
+            }
+            p++
+        }
+        return w1.slice(0, p)
+    }
+}
