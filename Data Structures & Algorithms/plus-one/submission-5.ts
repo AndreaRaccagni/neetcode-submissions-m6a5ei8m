93a9f39843a3e7d4993e2f2544617ecd8edd13c2
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-08-22 09:19:09 +0200

    Add: plus-one - submission-5

---
+++ Data Structures & Algorithms/plus-one/submission-5.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} digits
+     * @return {number[]}
+     */
+    plusOne(digits: number[]): number[] {
+        for (let i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] = digits[i] + 1
+                return digits
+            }
+            digits[i] = 0
+        }
+
+        return [1].concat(digits)
+
+    }
+}
