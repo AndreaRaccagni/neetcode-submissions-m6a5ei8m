f691108055f0b4bff5b538b8daef5f87e09c760b
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-08-31 21:43:36 +0200

    Add: buy-and-sell-crypto - submission-11

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-11.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let minPrice = Infinity
+        let maxProfit = 0
+
+        for (const p of prices) {
+            maxProfit = Math.max(maxProfit, p - minPrice)
+            minPrice = Math.min(minPrice, p)
+        }
+
+        return maxProfit
+    }
+}
