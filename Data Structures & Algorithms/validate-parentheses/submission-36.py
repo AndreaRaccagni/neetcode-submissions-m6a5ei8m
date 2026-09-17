d659c720d6825075ec5985741639c30b76d3485d
Andrea Raccagni <61575196+AndreaRaccagni@users.noreply.github.com>
2026-09-17 18:49:46 +0100

    Add: validate-parentheses - submission-36

---
+++ Data Structures & Algorithms/validate-parentheses/submission-36.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        par = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+
+        stack = []
+
+        for p in s:
+            if p in par:
+                if not stack or stack[-1] != par[p]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(p)
+
+        return not stack
